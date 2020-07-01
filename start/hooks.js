bd08972a52861b73f0459132fa96e28d0703030c
chukds <josuenzapadayoga@etu-digitalschool.paris>
2020-07-01 18:45:20 +0200

    feat: added admin, staff and user middleware.

--- start/hooks.js
+++ start/hooks.js
@@ -4,16 +4,21 @@ const { hooks } = require("@adonisjs/ignitor");
 
 hooks.after.providersBooted(() => {
   const View = use("Adonis/Src/View");
+  const Exception = use("Exception");
+  const Env = use("Env");
 
-  View.global("appUrl", path => {
-    const Env = use("Env");
-
+  View.global("appUrl", (path) => {
     const APP_URL = Env.get("APP_URL");
 
     return path ? `${APP_URL}/${path}` : APP_URL;
   });
 
-  View.global("encodeURIComponent", token => {
+  View.global("encodeURIComponent", (token) => {
     return encodeURIComponent(token);
+  });
+
+  // handle `InvalidSessionException`
+  Exception.handle("InvalidSessionException", (error, { response }) => {
+    return response.redirect("/login");
   });
 });
--- start/routes.js
+++ start/routes.js
@@ -18,77 +18,29 @@ const Route = use("Route");
 
 /*
 |--------------------------------------------------------------------------
-| Account Routes
-|--------------------------------------------------------------------------
-*/
-/*
-Route.group(() => {
-  Route.get("/account", "UserController.showEditAccount").as(
-    "settings.account"
-  );
-  Route.put("/account", "UserController.updateAccount");
-  Route.get("/password", "UserController.showChangePassword").as(
-    "settings.password"
-  );
-  Route.put("/password", "UserController.updatePassword");
-})
-  .prefix("/settings")
-  .middleware(["auth"]);
-*/
-/*
-Route.on("/admin")
-  .render("admin")
-  .middleware(["auth", "admin"]);
-*/
-
-/*
-|--------------------------------------------------------------------------
 | Admin Routes
 |--------------------------------------------------------------------------
 */
-
-// Show admin page
-/*Route.get("compte/admin", "CouponController.showAdminDashoard").as(
-  "admin.dashboard"
-);*/
-// Show admin home page
-Route.get("compte/admin", "AdminController.showAdminHome").as("admin-home");
-Route.get("compte/admin/stats", "AdminController.showAdminStats").as(
-  "admin-stats"
-);
 // Route.on("compte/admin/stats").render("admin.admin_stats").as("admin-stats");
 // Route.on("compte/admin/stats").render("admin.admin_stats").as("admin-stats");
 // Route.on("compte/admin").render("admin.admin_home").as("admin-home");
 
-// Show user information
-Route.post("compte/admin/tickets", "AdminController.userParticipation")
-  .validator("FindUser")
-  .as("admin.find.participation");
-
-// Show user participation
-Route.post("compte/admin/tickets", "AdminController.userParticipation")
-  .validator("FindUser")
-  .as("admin.find.participation");
-
-// Generate winner
-Route.post("compte/admin/champ", "AdminController.makeChamp").as(
-  "admin.make.champ"
-);
-
-// Find winnner
-Route.post("compte/admin/champ/trouver", "AdminController.findChamp").as(
-  "admin.find.champ"
-);
-
-// Download users
-Route.post("compte/admin/users/download", "AdminController.exportUsers").as(
-  "admin.users.export"
-);
-
-//Show user information
-/*Route.get("compte/admin/tickets", "AdminController.userParticipation").as(
-  "user-participation"
-);*/
+Route.group(() => {
+  Route.get("/", "AdminController.showAdminHome").as("admin-home");
+  Route.get("/stats", "AdminController.showAdminStats").as("admin-stats");
+  Route.post("/tickets", "AdminController.userParticipation")
+    .validator("FindUser")
+    .as("admin.find.participation");
+  Route.post("/champ", "AdminController.makeChamp").as("admin.make.champ");
+  Route.post("/champ/trouver", "AdminController.findChamp").as(
+    "admin.find.champ"
+  );
+  Route.post("/users/download", "AdminController.exportUsers").as(
+    "admin.users.export"
+  );
+})
+  .prefix("compte/admin")
+  .middleware(["auth", "admin"]);
 
 // Show list of staff
 // Show list of users
@@ -99,40 +51,20 @@ Route.post("compte/admin/users/download", "AdminController.exportUsers").as(
 |--------------------------------------------------------------------------
 */
 
-/*
-Route.get("register", "RegisterController.showRegister").middleware(["guest"]);
-Route.get("login", "LoginController.showLogin").middleware(["guest"]);
-*/
-
 // Register user
 Route.post("register", "AuthController.register")
   .validator("Register")
+  .middleware(["guest"])
   .as("register");
 
 // Login user
-Route.post("login", "AuthController.login").validator("Login").as("login");
+Route.post("login", "AuthController.login")
+  .validator("Login")
+  .middleware(["guest"])
+  .as("login");
 
 // Logout user
 Route.get("logout", "AuthController.logout").as("logout");
-
-/*
-|--------------------------------------------------------------------------
-| Demo Routes
-|--------------------------------------------------------------------------
-*/
-
-Route.on("demo").render("demo.home");
-Route.on("demo/contact").render("demo.contact");
-Route.on("demo/consent").render("demo.consent");
-Route.on("demo/dashboard").render("demo.dashboard");
-Route.on("demo/login").render("demo.login");
-Route.on("demo/register").render("demo.register");
-Route.on("demo/search").render("demo.search");
-Route.on("demo/tickets/add").render("demo.add-ticket");
-Route.on("demo/staff/tickets/list").render("demo.list-tickets");
-Route.on("demo/user").render("demo.user-home");
-Route.on("demo/admin/home").render("demo.admin-home");
-Route.on("demo/admin/stats").render("demo.admin-stats");
 
 /*
 |--------------------------------------------------------------------------
@@ -140,28 +72,17 @@ Route.on("demo/admin/stats").render("demo.admin-stats");
 |--------------------------------------------------------------------------
 */
 
-/*
-Route.get(
-  "password/reset",
-  "PasswordResetController.showLinkRequestForm"
-).middleware(["guest"]);
-*/
-
-// Show password reset request form
-
 // Send password reset link
 Route.post("password/email", "PasswordResetController.sendLinkReset")
   .validator("SendLink")
+  .middleware(["guest"])
   .as("password.email");
 
-/*
+// Show reset password page
 Route.get(
   "password/reset/:token",
   "PasswordResetController.showPasswordReset"
 ).middleware(["guest"]);
-*/
-// Show reset password page
-Route.get("password/reset/:token", "PasswordResetController.showPasswordReset");
 
 // Send password reset confirmation
 Route.post("password/reset", "PasswordResetController.sendPasswordConfirmation")
@@ -178,10 +99,16 @@ Route.post("password/reset", "PasswordResetController.sendPasswordConfirmation")
 Route.on("/").render("home").as("home");
 
 // Show staff home page
-Route.on("compte/equipe").render("staff.staff_home").as("staff-home");
+Route.on("compte/equipe")
+  .render("staff.staff_home")
+  .middleware(["staff"])
+  .as("staff-home");
 
 // Show user home page
-Route.on("compte/client").render("user.user_home").as("user-home");
+Route.on("compte/client")
+  .render("user.user_home")
+  .middleware(["user"])
+  .as("user-home");
 
 // Show consent page
 Route.on("consent").render("consent");
@@ -190,13 +117,19 @@ Route.on("consent").render("consent");
 Route.on("contact").render("contact");
 
 // Show login page
-Route.on("login").render("auth.login");
+// might cause error because of auth middleware !
+Route.on("login").render("auth.login").middleware(["guest"]);
 
 // Show register page
-Route.on("register").render("auth.register");
+Route.on("register").render("auth.register").middleware(["guest"]);
 
 // Show password reset request page
-Route.on("password/reset").render("password.email");
+Route.on("password/reset").render("password.email").middleware(["guest"]);
+
+// Demo Routes
+Route.on("demo/contact").render("demo.contact");
+Route.on("demo/consent").render("demo.consent");
+Route.on("demo/admin/stats").render("demo.admin-stats");
 
 /*
 |--------------------------------------------------------------------------
@@ -221,29 +154,29 @@ Route.get(
 | Staff Routes
 |--------------------------------------------------------------------------
 */
-// Find user coupons
-Route.post("compte/equipe/tickets", "StaffController.findCoupons")
-  .validator("FindCoupon")
-  .as("staff.find.coupons");
 
-// Activate user coupon
-Route.post("compte/equipe/tickets/:id", "StaffController.couponActivate");
-
+Route.group(() => {
+  Route.post("/tickets", "StaffController.findCoupons")
+    .validator("FindCoupon")
+    .as("staff.find.coupons");
+  Route.post("/tickets/:id", "StaffController.couponActivate");
+})
+  .prefix("compte/equipe")
+  .middleware(["auth", "staff"]);
 /*
 |--------------------------------------------------------------------------
 | User Routes
 |--------------------------------------------------------------------------
 */
 
-// Add user coupon
-Route.post("compte/client/tickets/ajouter", "UserController.addCoupon")
-  .validator("AddCoupon")
-  .as("user.addCoupon");
-
-// Show user coupons
-Route.get("compte/client/tickets", "UserController.userCoupons").as(
-  "user-tickets"
-);
+Route.group(() => {
+  Route.post("/tickets/ajouter", "UserController.addCoupon")
+    .validator("AddCoupon")
+    .as("user.addCoupon");
+  Route.get("/tickets", "UserController.userCoupons").as("user-tickets");
+})
+  .prefix("compte/client")
+  .middleware(["auth", "user"]);
 
 /*
 |--------------------------------------------------------------------------
